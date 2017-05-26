3d254ebc659d0c1999da6c6827ff51b90e092208
Smriti Das <dassmrit@gmail.com>
2017-05-26 15:36:24 -0400

    Add in Fragment.

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.popularmovies;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements MovieFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add in Fragment initiation
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+            //call the MovieFragment code
+            .add(R.id.movies_container,new MovieFragment())
+            .commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
