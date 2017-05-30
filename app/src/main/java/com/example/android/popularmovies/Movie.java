d1fd5c65c0d87cd90d00df9d339fc83360864192
Smriti Das <dassmrit@gmail.com>
2017-05-30 16:34:22 -0400

    Update Movie class with Parceable.

--- app/src/main/java/com/example/android/popularmovies/Movie.java
+++ app/src/main/java/com/example/android/popularmovies/Movie.java
@@ -1,12 +1,12 @@
 package com.example.android.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-/**
- * Created by SmritiDas on 2017-05-29.
- */
 
-public class Movie {
+public class Movie implements Parcelable{
 
     //Added LOG tag for debugging
     private static final String LOG_TAG = Movie.class.getSimpleName();
@@ -16,15 +16,39 @@ public class Movie {
     private String mTrailerPrimaryUri;
     private String mThumbnail;
 
-    private List<Movie> Movies;
 
-    Movie(String strTitle, String strYear, String strTrailer, String strThumbnail){
-        this.mTitle = strTitle;
-        this.mYear = strYear;
-        this.mTrailerPrimaryUri = strTrailer;
-        this.mThumbnail = strThumbnail;
+    private Movie(Parcel in){
+        this.mTitle = in.readString();
+        this.mYear = in.readString();
+        this.mTrailerPrimaryUri = in.readString();
+        this.mThumbnail = in.readString();
 
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flag) {
+        dest.writeString(this.mTitle);
+        dest.writeString(this.mYear);
+        dest.writeString(this.mTrailerPrimaryUri);
+        dest.writeString(this.mThumbnail);
+
+    }
+
+    public final Parcelable.Creator<Movie> CREATOR = new
+            Creator<Movie>() {
+                @Override
+                public Movie createFromParcel(Parcel parcel) {
+                    return new Movie(parcel);
+                }
+
+                @Override
+                public Movie[] newArray(int i) {
+                    return new Movie[0];
+                }
+            };
 }
